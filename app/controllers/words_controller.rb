0fb3a84174e6ec56ed6c713f5498f93a47ec36e3
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-06-04 14:17:31 -0400

    word form partial and new views to create a new word and add to a list. words are only created once but can be linked to multiple vocab lists

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,3 +1,32 @@
-class WordController < ApplicationController
+class WordsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+    @word = Word.new
+  end
+
+  def create
+    @word = Word.find_or_create_by(word_name: word_params[:word_name].downcase)
+    @list = VocabList.find(word_params[:vocab_lists].to_i)
+    if @list.words.include? @word
+      flash[:notice] = "That word is already in this list."
+    else
+      @word.vocab_lists.push(@list)
+      @word.dictionary_lookup
+    end
+    redirect_to @list
+  end
+
+  private
+  def word_params
+    params.require(:word).permit(:word_name, :vocab_lists)
+  end
 
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,20 @@
-class Word < ActiveRecord::base
+class Word < ActiveRecord::Base
+  has_and_belongs_to_many :vocab_lists
+
+  DICTIONARY_API = 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml/'
+
+  def dictionary_lookup
+    # binding.pry
+    result = HTTParty.get(DICTIONARY_API + self.word_name + "?key=" + ENV['DICTIONARY_API_KEY'])
+    json = Crack::XML.parse(result)
+    if json["entry_list"]["entry"].length > 1
+      self.part_of_speech = json["entry_list"]["entry"][0]["fl"]
+      self.definition = json["entry_list"]["entry"][0]["def"]["dt"][0]
+    else
+      self.part_of_speech = json["entry_list"]["entry"]["fl"]
+      self.definition = json["entry_list"]["entry"]["def"]["dt"][0]
+    end
+    self.save!
+  end
 
 end
