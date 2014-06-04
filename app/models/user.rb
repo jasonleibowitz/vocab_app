44b8a7fb42a3cf208d000f0c9b7418086f532085
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-06-04 14:19:00 -0400

    user model created and basic show page created

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :omniauthable, :omniauth_providers => [:facebook]
 
+  # has_and_belongs_to_many :vocab_lists
+  has_many :vocab_lists
+
   def self.find_for_facebook_oauth(auth)
     where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
       user.image = auth.info.image # assuming the user model has an image
     end
   end
