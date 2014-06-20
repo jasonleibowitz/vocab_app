9e9b77ef9f9e4a9a88baea6f192e214d45d716da
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-06-20 19:07:08 -0400

    updated to work with wordnik api

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,52 +1,18 @@
 class Word < ActiveRecord::Base
   has_and_belongs_to_many :vocab_lists
 
-  DICTIONARY_API = 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml/'
+  WORDNIK_API_PREFIX = 'http://api.wordnik.com:80/v4/word.json/'
+  WORDNIK_API_SUFFIX = '/definitions?limit=20&includeRelated=true&sourceDictionaries=wiktionary&useCanonical=true&includeTags=false&api_key='
 
   def dictionary_lookup
-    result = HTTParty.get(DICTIONARY_API + self.word_name + "?key=" + ENV['DICTIONARY_API_KEY'])
-    json = Crack::XML.parse(result)
-    if json["entry_list"]["suggestion"] == nil
-      begin
-        self.part_of_speech = json["entry_list"]["entry"][0]["fl"]
-        if json["entry_list"]["entry"][0]["def"]["dt"].is_a? Array
-          self.definition = json["entry_list"]["entry"][0]["def"]["dt"][0]
-        else
-          self.definition = json["entry_list"]["entry"][0]["def"]["dt"]
-        end
-        self.sub_words
-      rescue NoMethodError
-        self.part_of_speech = json["entry_list"]["entry"]["fl"]
-        if json["entry_list"]["entry"]["def"]["dt"].is_a? Array
-          self.definition = json["entry_list"]["entry"]["def"]["dt"][0]
-        else
-          self.definition = json["entry_list"]["entry"]["def"]["dt"]
-        end
-        self.sub_words
-      end
-      self.save!
-      return 'successful'
+    result = HTTParty.get(WORDNIK_API_PREFIX + self.word_name + WORDNIK_API_SUFFIX + ENV['WORDNIK_API_KEY'])
+    if result.first == nil
+      return false
     else
-      self.destroy!
-      return json["entry_list"]["suggestion"]
+      self.part_of_speech = result.first["partOfSpeech"]
+      self.definition = result.first["text"]
+      return "successful"
     end
-  end
-
-  def sub_words
-    self.definition.gsub!(":", "")
-    self.definition.gsub!("<vi>", "")
-    self.definition.gsub!("</vi>", "")
-    self.definition.gsub!("<it>", "")
-    self.definition.gsub!("</it>", "")
-    self.definition.gsub!("<aq>", "")
-    self.definition.gsub!("</aq>", "")
-    self.definition.gsub!("<sx>", ", ")
-    self.definition.gsub!("</sx>", ". ")
-    self.definition.gsub!("<d_link>", "")
-    self.definition.gsub!("</d_link>", "")
-    self.definition.gsub!("<fw>", "")
-    self.definition.gsub!("</fw>", "")
-    return self
   end
 
   def update_list_count
