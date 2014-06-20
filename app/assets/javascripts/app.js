e0bf4da8a2be8c045a810515d753ddebfab0a26e
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-06-20 17:23:25 -0400

    fixed js for card flip

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 
   console.log('loaded bro!');
+  function toggleMenu() {
+    var card = $(this).find('.card');
+    if ($(this).find('.card').hasClass('flipped')) {
+      card.removeClass('flipped');
+      console.log('unflipped');
+    } else {
+      card.addClass('flipped');
+      console.log('flipped');
+    }
+  }
+
+  $('#js-flip-1').click(toggleMenu);
+  $('.flip').each(function(i, v) {
+    $(v).click(toggleMenu);
+  });
+
 
 });
