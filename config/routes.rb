a368eb0ca61ec4dc689f564db1139149e69e97ff
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-06-20 19:31:28 -0400

    added popular route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
   resources :users
   resources :vocab_lists
+  get '/words/popular' => 'words#popular'
   resources :words
   get '/test' => 'vocab_lists#test'
   root to: 'users#show'
