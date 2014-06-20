f43674d2ee9ec9ae4fb9e54e81428b77a1db0507
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-06-20 17:23:53 -0400

    fixed show page to make card flip work

--- app/assets/javascripts/vocab_show.js
+++ app/assets/javascripts/vocab_show.js
@@ -1,9 +1,28 @@
 $(document).ready(function(){
   console.log("voacab loaded bro!");
 
-  for(i=0; i < window.cards.length; i++){
-    $('#word_' + window.cards[i]).flip();
-  }
+  // $('.wrap-wrap').each(function(i, v) {
+  //   $(v).flip();
+  // });
+
+  // $('.card-wrap').each(function(index, value) {
+  //   if ($(value).data('flipped') === true){
+  //     $(this).flipOn();
+  //   } else {
+  //     $(this).flipOff();
+  //   }
+  //   // $(value).flip();
+  // });
 
 
 });
+
+function flipOn(){
+  $(this).flip(true);
+  $(this).data('flipped', true);
+}
+
+function flipOff(){
+  $(this).flip(false);
+  $(this).data('flipped', false);
+}
