48f39029c08ed5f21213f135ae6d41a44e10b6af
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-06-25 13:51:10 -0400

    added game route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
   resources :users
+  get 'vocab_lists/game/:id' => 'vocab_lists#game', as: 'game'
   resources :vocab_lists
   get '/words/popular' => 'words#popular'
   resources :words
